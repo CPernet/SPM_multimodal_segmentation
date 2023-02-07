198dd6863176f8521e562550cbd0f29051139d60
kiwimarc <marccummings@live.dk>
2023-02-07 16:22:08 +0100

    Update segment_images.m

--- code/segment_images.m
+++ code/segment_images.m
@@ -205,9 +205,9 @@ for subject=3:(N+2)
     end
 end
 temp_name = ['volumes' options.modality '_nG' num2str(options.NGaussian)];
-save([outdir filesep temp_name],'volumes','-v7.3')
+save(outdir+filesep+temp_name,'volumes','-v7.3')
 temp_name = ['distrib' options.modality '_nG' num2str(options.NGaussian)];
-save([outdir filesep temp_name],'distrib','-v7.3')
+save(outdir+filesep+temp_name,'distrib','-v7.3')
 clear distrib volumes
 
 %% generate the DARTEL template
