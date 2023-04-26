85c52da5a726fa34b1d4a7248d988fa7f8bf728a
kiwimarc <marccummings@live.dk>
2023-04-26 09:40:15 +0200

    Added plotting for the rest of the tissue metrics

--- code/multispectral_segmentation_analysis.m
+++ code/multispectral_segmentation_analysis.m
@@ -132,12 +132,11 @@ T12_nG2 = [LowerConfs(:,4) TrimmedMeans(:,4) HigherConfs(:,4)];
 T = table(T1_nG1,T1_nG2,T12_nG1,T12_nG2,...
     'RowNames',RowNames);
 writetable(T,'Volumes_TrimmedMeans.csv','WriteRowNames',true);
-
 clear GM_est WM_est CSF_est CI_GM CI_WM CI_CSF TrimmedMeans LowerConfs HigherConfs T1_nG1 T1_nG2 T12_nG1 T12_nG2 T
 
 % Multi compare between the 4 conditions (T1_nG1 vs T1_nG2, T12_nG1 vs T12_nG2, T1_nG1 vs T12_nG1, T1_nG2 vs T12_nG2)
-[diff_GM,CI_GM,p_GM,alphav_GM,h_GM] = rst_multicompare(volumes_GM,[1 2; 3 4; 1 3; 2 4], 'estimator', 'trimmed mean','newfig','yes');
-[diff_WM,CI_WM,p_WM,alphav_WM,h_WM] = rst_multicompare(volumes_WM,[1 2; 3 4; 1 3; 2 4], 'estimator', 'trimmed mean','newfig','yes');
+[diff_GM,CI_GM,p_GM,alphav_GM,h_GM]      = rst_multicompare(volumes_GM,[1 2; 3 4; 1 3; 2 4], 'estimator', 'trimmed mean','newfig','yes');
+[diff_WM,CI_WM,p_WM,alphav_WM,h_WM]      = rst_multicompare(volumes_WM,[1 2; 3 4; 1 3; 2 4], 'estimator', 'trimmed mean','newfig','yes');
 [diff_CSF,CI_CSF,p_CSF,alphav_CSF,h_CSF] = rst_multicompare(volumes_CSF,[1 2; 3 4; 1 3; 2 4], 'estimator', 'trimmed mean','newfig','yes');
 if(debug)
     % save plot for Multi compare and close figure
@@ -156,14 +155,10 @@ PValues             = [p_GM,p_WM, p_CSF];
 AlphaValues         = [alphav_GM; alphav_WM; alphav_CSF];
 Significances       = [h_GM; h_WM; h_CSF];
 
-T1_nG1  = [LowerConfs(:,1) PairwiseDifferences(:,1) HigherConfs(:,1)];
-T1_nG2  = [LowerConfs(:,2) PairwiseDifferences(:,2) HigherConfs(:,2)];
-T12_nG1 = [LowerConfs(:,3) PairwiseDifferences(:,3) HigherConfs(:,3)];
-T12_nG2 = [LowerConfs(:,4) PairwiseDifferences(:,4) HigherConfs(:,4)];
+clear diff_GM diff_WM diff_CSF CI_GM CI_WM CI_CSF PairwiseDifferences LowerConfs HigherConfs PValues AlphaValues Significances T
 
-T = table(TrimmedMeans,TrimmedMeansHDIs,...
-    'RowNames',RowNames);
-writetable(T,'Volumes_TrimmedMeans.csv','WriteRowNames',true);
+% HD
+load('HD.mat');
 
 % entropy
 load('entropyT1_nG1.mat');  T1_nG1_entropy  = entropy; clear entropy
@@ -171,17 +166,180 @@ load('entropyT1_nG2.mat');  T1_nG2_entropy  = entropy; clear entropy
 load('entropyT12_nG1.mat'); T12_nG1_entropy = entropy; clear entropy
 load('entropyT12_nG2.mat'); T12_nG2_entropy = entropy; clear entropy
 
+entropy_GM  = [T1_nG1_entropy(:,1) T1_nG2_entropy(:,1) T12_nG1_entropy(:,1) T12_nG2_entropy(:,1)];
+entropy_WM  = [T1_nG1_entropy(:,2) T1_nG2_entropy(:,2) T12_nG1_entropy(:,2) T12_nG2_entropy(:,2)];
+entropy_CSF = [T1_nG1_entropy(:,3) T1_nG2_entropy(:,3) T12_nG1_entropy(:,3) T12_nG2_entropy(:,3)];
+
+[GM_est, CI_GM]   = rst_data_plot(entropy_GM, 'estimator','trimmed mean');
+[WM_est, CI_WM]   = rst_data_plot(entropy_WM, 'estimator','trimmed mean','newfig','yes');
+[CSF_est, CI_CSF] = rst_data_plot(entropy_CSF, 'estimator','trimmed mean','newfig','yes');
+if(debug)
+    % save plot for entropy trimmed mean and close figure
+    saveas(figure(1), "GM_entropy_TM.png"); 
+    saveas(figure(2), "WM_entropy_TM.png");
+    saveas(figure(3), "CSF_entropy_TM.png");
+end
+close(figure(1));
+close(figure(2));
+close(figure(3));
+
+TrimmedMeans = [GM_est; WM_est; CSF_est];
+LowerConfs   = [CI_GM(1,:); CI_WM(1,:); CI_CSF(1,:)];
+HigherConfs  = [CI_GM(2,:); CI_WM(2,:); CI_CSF(2,:)];
+
+T1_nG1  = [LowerConfs(:,1) TrimmedMeans(:,1) HigherConfs(:,1)];
+T1_nG2  = [LowerConfs(:,2) TrimmedMeans(:,2) HigherConfs(:,2)];
+T12_nG1 = [LowerConfs(:,3) TrimmedMeans(:,3) HigherConfs(:,3)];
+T12_nG2 = [LowerConfs(:,4) TrimmedMeans(:,4) HigherConfs(:,4)];
+
+T = table(T1_nG1,T1_nG2,T12_nG1,T12_nG2,...
+    'RowNames',RowNames);
+writetable(T,'entropy_TrimmedMeans.csv','WriteRowNames',true);
+clear GM_est WM_est CSF_est CI_GM CI_WM CI_CSF TrimmedMeans LowerConfs HigherConfs T1_nG1 T1_nG2 T12_nG1 T12_nG2 T
+
+% Multi compare between the 4 conditions (T1_nG1 vs T1_nG2, T12_nG1 vs T12_nG2, T1_nG1 vs T12_nG1, T1_nG2 vs T12_nG2)
+[diff_GM,CI_GM,p_GM,alphav_GM,h_GM]      = rst_multicompare(entropy_GM,[1 2; 3 4; 1 3; 2 4], 'estimator', 'trimmed mean','newfig','yes');
+[diff_WM,CI_WM,p_WM,alphav_WM,h_WM]      = rst_multicompare(entropy_WM,[1 2; 3 4; 1 3; 2 4], 'estimator', 'trimmed mean','newfig','yes');
+[diff_CSF,CI_CSF,p_CSF,alphav_CSF,h_CSF] = rst_multicompare(entropy_CSF,[1 2; 3 4; 1 3; 2 4], 'estimator', 'trimmed mean','newfig','yes');
+if(debug)
+    % save plot for Multi compare and close figure
+    saveas(figure(1), "MultiComp_GM_vol.png");
+    saveas(figure(2), "MultiComp_WM_vol.png");
+    saveas(figure(3), "MultiComp_CSF_vol.png");
+end
+close(figure(1));
+close(figure(2));
+close(figure(3));
+
+PairwiseDifferences = [diff_GM; diff_WM; diff_CSF];
+LowerConfs          = [CI_GM(1,:); CI_WM(1,:); CI_CSF(1,:)];
+HigherConfs         = [CI_GM(2,:); CI_WM(2,:); CI_CSF(2,:)];
+PValues             = [p_GM,p_WM, p_CSF];
+AlphaValues         = [alphav_GM; alphav_WM; alphav_CSF];
+Significances       = [h_GM; h_WM; h_CSF];
+
+clear diff_GM diff_WM diff_CSF CI_GM CI_WM CI_CSF PairwiseDifferences LowerConfs HigherConfs PValues AlphaValues Significances T
+
+
 % dunnIndex
 load('dunnIndexT1_nG1.mat');  T1_nG1_dunnIndex  = dunnIndexes; clear dunnIndexes
 load('dunnIndexT1_nG2.mat');  T1_nG2_dunnIndex  = dunnIndexes; clear dunnIndexes
 load('dunnIndexT12_nG1.mat'); T12_nG1_dunnIndex = dunnIndexes; clear dunnIndexes
 load('dunnIndexT12_nG2.mat'); T12_nG2_dunnIndex = dunnIndexes; clear dunnIndexes
 
+dunnIndex_GM  = [T1_nG1_dunnIndex(:,1) T1_nG2_dunnIndex(:,1) T12_nG1_dunnIndex(:,1) T12_nG2_dunnIndex(:,1)];
+dunnIndex_WM  = [T1_nG1_dunnIndex(:,2) T1_nG2_dunnIndex(:,2) T12_nG1_dunnIndex(:,2) T12_nG2_dunnIndex(:,2)];
+dunnIndex_CSF = [T1_nG1_dunnIndex(:,3) T1_nG2_dunnIndex(:,3) T12_nG1_dunnIndex(:,3) T12_nG2_dunnIndex(:,3)];
+
+[GM_est, CI_GM]   = rst_data_plot(dunnIndex_GM, 'estimator','trimmed mean');
+[WM_est, CI_WM]   = rst_data_plot(dunnIndex_WM, 'estimator','trimmed mean','newfig','yes');
+[CSF_est, CI_CSF] = rst_data_plot(dunnIndex_CSF, 'estimator','trimmed mean','newfig','yes');
+if(debug)
+    % save plot for dunnIndex trimmed mean and close figure
+    saveas(figure(1), "GM_dunnIndex_TM.png"); 
+    saveas(figure(2), "WM_dunnIndex_TM.png");
+    saveas(figure(3), "CSF_dunnIndex_TM.png");
+end
+close(figure(1));
+close(figure(2));
+close(figure(3));
+
+TrimmedMeans = [GM_est; WM_est; CSF_est];
+LowerConfs   = [CI_GM(1,:); CI_WM(1,:); CI_CSF(1,:)];
+HigherConfs  = [CI_GM(2,:); CI_WM(2,:); CI_CSF(2,:)];
+
+T1_nG1  = [LowerConfs(:,1) TrimmedMeans(:,1) HigherConfs(:,1)];
+T1_nG2  = [LowerConfs(:,2) TrimmedMeans(:,2) HigherConfs(:,2)];
+T12_nG1 = [LowerConfs(:,3) TrimmedMeans(:,3) HigherConfs(:,3)];
+T12_nG2 = [LowerConfs(:,4) TrimmedMeans(:,4) HigherConfs(:,4)];
+
+T = table(T1_nG1,T1_nG2,T12_nG1,T12_nG2,...
+    'RowNames',RowNames);
+writetable(T,'dunnIndex_TrimmedMeans.csv','WriteRowNames',true);
+clear GM_est WM_est CSF_est CI_GM CI_WM CI_CSF TrimmedMeans LowerConfs HigherConfs T1_nG1 T1_nG2 T12_nG1 T12_nG2 T
+
+% Multi compare between the 4 conditions (T1_nG1 vs T1_nG2, T12_nG1 vs T12_nG2, T1_nG1 vs T12_nG1, T1_nG2 vs T12_nG2)
+[diff_GM,CI_GM,p_GM,alphav_GM,h_GM]      = rst_multicompare(dunnIndex_GM,[1 2; 3 4; 1 3; 2 4], 'estimator', 'trimmed mean','newfig','yes');
+[diff_WM,CI_WM,p_WM,alphav_WM,h_WM]      = rst_multicompare(dunnIndex_WM,[1 2; 3 4; 1 3; 2 4], 'estimator', 'trimmed mean','newfig','yes');
+[diff_CSF,CI_CSF,p_CSF,alphav_CSF,h_CSF] = rst_multicompare(dunnIndex_CSF,[1 2; 3 4; 1 3; 2 4], 'estimator', 'trimmed mean','newfig','yes');
+if(debug)
+    % save plot for Multi compare and close figure
+    saveas(figure(1), "MultiComp_GM_vol.png");
+    saveas(figure(2), "MultiComp_WM_vol.png");
+    saveas(figure(3), "MultiComp_CSF_vol.png");
+end
+close(figure(1));
+close(figure(2));
+close(figure(3));
+
+PairwiseDifferences = [diff_GM; diff_WM; diff_CSF];
+LowerConfs          = [CI_GM(1,:); CI_WM(1,:); CI_CSF(1,:)];
+HigherConfs         = [CI_GM(2,:); CI_WM(2,:); CI_CSF(2,:)];
+PValues             = [p_GM,p_WM, p_CSF];
+AlphaValues         = [alphav_GM; alphav_WM; alphav_CSF];
+Significances       = [h_GM; h_WM; h_CSF];
+
+clear diff_GM diff_WM diff_CSF CI_GM CI_WM CI_CSF PairwiseDifferences LowerConfs HigherConfs PValues AlphaValues Significances T
+
 % distrib
 load('distribT1_nG1.mat');  T1_nG1_distrib  = distrib; clear distrib
 load('distribT1_nG2.mat');  T1_nG2_distrib  = distrib; clear distrib
 load('distribT12_nG1.mat'); T12_nG1_distrib = distrib; clear distrib
 load('distribT12_nG2.mat'); T12_nG2_distrib = distrib; clear distrib
+
+tissue_distrib_GM  = [T1_nG1_distrib(:,1) T1_nG2_distrib(:,1) T12_nG1_distrib(:,1) T12_nG2_distrib(:,1)];
+tissue_distrib_WM  = [T1_nG1_distrib(:,2) T1_nG2_distrib(:,2) T12_nG1_distrib(:,2) T12_nG2_distrib(:,2)];
+tissue_distrib_CSF = [T1_nG1_distrib(:,3) T1_nG2_distrib(:,3) T12_nG1_distrib(:,3) T12_nG2_distrib(:,3)];
+
+[GM_est, CI_GM]   = rst_data_plot(tissue_distrib_GM, 'estimator','trimmed mean');
+[WM_est, CI_WM]   = rst_data_plot(tissue_distrib_WM, 'estimator','trimmed mean','newfig','yes');
+[CSF_est, CI_CSF] = rst_data_plot(tissue_distrib_CSF, 'estimator','trimmed mean','newfig','yes');
+if(debug)
+    % save plot for tissue distrib trimmed mean and close figure
+    saveas(figure(1), "GM_tissue_distrib_TM.png"); 
+    saveas(figure(2), "WM_tissue_distrib_TM.png");
+    saveas(figure(3), "CSF_tissue_distrib_TM.png");
+end
+close(figure(1));
+close(figure(2));
+close(figure(3));
+
+TrimmedMeans = [GM_est; WM_est; CSF_est];
+LowerConfs   = [CI_GM(1,:); CI_WM(1,:); CI_CSF(1,:)];
+HigherConfs  = [CI_GM(2,:); CI_WM(2,:); CI_CSF(2,:)];
+
+T1_nG1  = [LowerConfs(:,1) TrimmedMeans(:,1) HigherConfs(:,1)];
+T1_nG2  = [LowerConfs(:,2) TrimmedMeans(:,2) HigherConfs(:,2)];
+T12_nG1 = [LowerConfs(:,3) TrimmedMeans(:,3) HigherConfs(:,3)];
+T12_nG2 = [LowerConfs(:,4) TrimmedMeans(:,4) HigherConfs(:,4)];
+
+T = table(T1_nG1,T1_nG2,T12_nG1,T12_nG2,...
+    'RowNames',RowNames);
+writetable(T,'tissue_distrib_TrimmedMeans.csv','WriteRowNames',true);
+clear GM_est WM_est CSF_est CI_GM CI_WM CI_CSF TrimmedMeans LowerConfs HigherConfs T1_nG1 T1_nG2 T12_nG1 T12_nG2 T
+
+% Multi compare between the 4 conditions (T1_nG1 vs T1_nG2, T12_nG1 vs T12_nG2, T1_nG1 vs T12_nG1, T1_nG2 vs T12_nG2)
+[diff_GM,CI_GM,p_GM,alphav_GM,h_GM] = rst_multicompare(tissue_distrib_GM,[1 2; 3 4; 1 3; 2 4], 'estimator', 'trimmed mean','newfig','yes');
+[diff_WM,CI_WM,p_WM,alphav_WM,h_WM] = rst_multicompare(tissue_distrib_WM,[1 2; 3 4; 1 3; 2 4], 'estimator', 'trimmed mean','newfig','yes');
+[diff_CSF,CI_CSF,p_CSF,alphav_CSF,h_CSF] = rst_multicompare(tissue_distrib_CSF,[1 2; 3 4; 1 3; 2 4], 'estimator', 'trimmed mean','newfig','yes');
+if(debug)
+    % save plot for Multi compare and close figure
+    saveas(figure(1), "MultiComp_GM_vol.png");
+    saveas(figure(2), "MultiComp_WM_vol.png");
+    saveas(figure(3), "MultiComp_CSF_vol.png");
+end
+close(figure(1));
+close(figure(2));
+close(figure(3));
+
+PairwiseDifferences = [diff_GM; diff_WM; diff_CSF];
+LowerConfs          = [CI_GM(1,:); CI_WM(1,:); CI_CSF(1,:)];
+HigherConfs         = [CI_GM(2,:); CI_WM(2,:); CI_CSF(2,:)];
+PValues             = [p_GM,p_WM, p_CSF];
+AlphaValues         = [alphav_GM; alphav_WM; alphav_CSF];
+Significances       = [h_GM; h_WM; h_CSF];
+
+clear diff_GM diff_WM diff_CSF CI_GM CI_WM CI_CSF PairwiseDifferences LowerConfs HigherConfs PValues AlphaValues Significances T
 
 % distrib_vessels
 load('distrib_vesselsT1_nG1.mat');  T1_nG1_distrib_vessels  = distrib_vessels; clear distrib_vessels
@@ -195,7 +353,6 @@ load('distrib_nucleiT1_nG1.mat');  T1_nG1_distrib_nuclei  = distrib_nuclei; clea
 load('distrib_nucleiT1_nG2.mat');  T1_nG2_distrib_nuclei  = distrib_nuclei; clear distrib_nuclei
 load('distrib_nucleiT12_nG1.mat'); T12_nG1_distrib_nuclei = distrib_nuclei; clear distrib_nuclei
 load('distrib_nucleiT12_nG2.mat'); T12_nG2_distrib_nuclei = distrib_nuclei; clear distrib_nuclei
-
 
 
 %% Compare derived volumes between the 4 segmentations
